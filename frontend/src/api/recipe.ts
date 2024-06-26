742ba9784831414a766109d18899b368b39d7bef
almog <almog@ummanu.health>
2024-06-26 10:42:11 +0300

    fix images

--- frontend/src/api/recipe.ts
+++ frontend/src/api/recipe.ts
@@ -2,35 +2,38 @@ import { apiService } from ".";
 import axios from "axios";
 import { Recipe } from "../../../backend/src/recipe/entities/recipe.entity";
 
+interface UnsplashResponse {
+  results: { urls: { regular: string } }[];
+}
+
 export const getAllRecipes = async () =>{
 
     return await apiService.get('/recipe')
 
 }
-export const postAllPics = async (recipes: Recipe[]) => {
-    try {
-      const imgPromise = recipes.map(async (recipe) => {
-        const response = await axios.post('https://api.openai.com/v1/images/generations', {
-          prompt: recipe.title,
-          n: 1,
-          size: "1024x1024"
-        }, {
-          headers: {
-            'Authorization': `test`,  
-            'Content-Type': 'application/json'
-          }
-        });
-        console.log(response.data ,"this is data ");
-        
-        return response.data.data[0].url;
-      });
-  
-      const imageUrls = await Promise.all(imgPromise);
-      return { data: imageUrls};
-    } catch (error) {
-      console.error("Error generating images: ", error);
-      throw new Error("Failed to generate images");
-    }
-  };
 
+
+
+export const postAllPics = async (recipes: Recipe[]): Promise<{ data: string[] }> => {
+  try {
+    const imgPromise = recipes.map(async (recipe) => {
+      const response = await axios.get<UnsplashResponse>('https://api.unsplash.com/search/photos', {
+        params: {
+          query: recipe.title,
+          per_page: 1
+        },
+        headers: {
+          'Authorization': `Client-ID ${import.meta.env.VITE_UNSPLASH_ACCESS_KEY}`, // Use environment variable
+        }
+      });
+      return response.data.results[0]?.urls.regular;
+    });
+
+    const imageUrls = await Promise.all(imgPromise);
+    return { data: imageUrls };
+  } catch (error) {
+    console.error('Error fetching images: ',error);
+    throw new Error('Failed to fetch images');
+  }
+};
 
